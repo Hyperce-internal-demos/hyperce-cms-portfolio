c6ea3f5b9e442615ca2c1755b1aa2d23758ca85b
bibekshhh <bibekrock2019@gmail.com>
2023-08-29 20:46:23 +0545

    deploy strapi backend

---
+++ backend-cms/src/admin/app.js
@@ -0,0 +1,18 @@
+
+export default {
+  config: {
+    locales: ['en'],
+    translations: {
+      en: {
+        "Auth.form.button.login.strapi": "Log in via Hyperce",
+        'Auth.form.welcome.title': 'Welcome to Hyperce',
+        "Auth.form.welcome.subtitle": "Log in to your Hyperce account",
+        "app.components.LeftMenu.navbrand.title": "Hyperce Dashboard",
+        "Auth.form.register.subtitle": "Credentials are only used to authenticate in Hyperce. All saved data will be stored in your database.",
+        "HomePage.welcome.congrats.content": "You are logged in as the first administrator. To discover the powerful features provided by Hyperce,",
+        "Settings.permissions.users.listview.header.subtitle": "All the users who have access to the Hyperce admin panel"
+      },
+    },
+  },
+  bootstrap() {},
+};
