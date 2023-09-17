a7834c7da43a2040d6ccd8a9276d507be9fc8ae2
bibekshhh <bibekrock2019@gmail.com>
2023-09-17 19:38:20 +0545

    [add]: push build && node modules for deployment

---
+++ backend-cms/.cache/admin/src/app.js
@@ -0,0 +1,31 @@
+
+export default {
+  config: {
+    locales: ['en'],
+    translations: {
+      en: {
+        "app.components.BlockLink.blog": "",
+        "app.components.BlockLink.blog.content": "",
+        "app.components.BlockLink.cloud": "",
+        "app.components.BlockLink.cloud.content": "",
+        "app.components.BlockLink.code": "",
+        "app.components.BlockLink.code.content": "",
+        "app.components.BlockLink.documentation.content": "",
+        "app.components.BlockLink.tutorial": "",
+        "app.components.BlockLink.tutorial.content": "",
+
+        // -------
+        "Auth.form.button.login.strapi": "Log in via Hyperce",
+        'Auth.form.welcome.title': 'Welcome to Hyperce',
+        "Auth.form.welcome.subtitle": "Log in to your Hyperce account",
+        "app.components.LeftMenu.navbrand.title": "Hyperce Dashboard",
+        "Auth.form.register.subtitle": "Credentials are only used to authenticate in Hyperce. All saved data will be stored in your database.",
+        "HomePage.welcome.congrats.content": "You are logged in as the first administrator. To discover the powerful features provided by Hyperce,",
+        "Settings.permissions.users.listview.header.subtitle": "All the users who have access to the Hyperce admin panel"
+      },
+    },
+    tutorials: false,
+    notifications: { releases: false },
+  },
+  bootstrap() {},
+};
